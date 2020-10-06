ef81feac815da5b215f55a85945874aa89f443e0
Karim Ahmadi <ahmadi.k@tiddev.com>
2020-10-06 15:23:06 +0330

    fix inquirer

--- bin/lib/inquirer.js
+++ bin/lib/inquirer.js
@@ -1,33 +1,31 @@
-const inquirer = require('inquirer');
+import inquirer from 'inquirer';
 
 module.exports = {
-    askComponentProps: () => {
-        const questions = [
-            {
-                name: 'name',
-                type: 'input',
-                message: 'Enter name of your Component:',
-                validate: function( value ) {
-                    if (value.length) {
-                        return true;
-                    } else {
-                        return 'Please enter name of Component.';
-                    }
-                }
-            },
-            {
-                name: 'description',
-                type: 'input',
-                message: 'Enter description of Component:',
-                validate: function(value) {
-                    if (value.length) {
-                        return true;
-                    } else {
-                        return 'Please enter description of Component.';
-                    }
-                }
-            }
-        ];
-        return inquirer.prompt(questions);
-    },
+  askComponentProps: () => {
+    const questions = [
+      {
+        name: 'name',
+        type: 'input',
+        message: 'Enter name of your Component:',
+        validate(value) {
+          if (value.length) {
+            return true;
+          }
+          return 'Please enter name of Component.';
+        },
+      },
+      {
+        name: 'description',
+        type: 'input',
+        message: 'Enter description of Component:',
+        validate(value) {
+          if (value.length) {
+            return true;
+          }
+          return 'Please enter description of Component.';
+        },
+      },
+    ];
+    return inquirer.prompt(questions);
+  },
 };
