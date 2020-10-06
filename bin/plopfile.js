08d56c5106239dc89e31d3e79ad9080d0c7fdd17
Karim Ahmadi <ahmadi.k@tiddev.com>
2020-10-06 14:26:27 +0330

    test pre-commit

--- bin/plopfile.js
+++ bin/plopfile.js
@@ -1,63 +1,64 @@
 const path = require('path');
 const pkg = require('../package.json');
 
-module.exports = function (plop) {
-    plop.setHelper('pkgVersion', function () {
-        return pkg.version;
-    });
-    plop.setGenerator('CreateComponent', {
-        description: 'this is a skeleton plopfile',
-        prompts: [{
-            type: 'input',
-            name: 'name',
-            message: 'Please enter a name for your component:'
-        }],
-        actions: [
-
-            {
-                type: 'add',
-                path: path.join(process.cwd(), '{{name}}', '/src/lib/index.js'),
-                templateFile: 'templates/component.hbs'
-            },
-            {
-                type: 'add',
-                path: path.join(process.cwd(), '{{name}}', '/src/index.js'),
-                templateFile: 'templates/index.hbs'
-            },
-            {
-                type: 'add',
-                path: 'source/.npmignore',
-                templateFile: 'templates/npmignore.hbs'
-            },
-            {
-                type: 'add',
-                path: 'source/.gitignore',
-                templateFile: 'templates/gitignore.hbs'
-            },
-            {
-                type: 'add',
-                path: 'source/package.json',
-                templateFile: 'templates/package.hbs'
-            },
-            {
-                type: 'add',
-                path: 'source/webpack.config.js',
-                templateFile: 'templates/webpack.config.hbs'
-            },
-            {
-                type: 'add',
-                path: 'source/.reactseamrc',
-                templateFile: 'templates/reactseamrc.hbs'
-            },
-            {
-                type: 'addMany',
-                destination: path.join(process.cwd(), '{{name}}'),
-                base: 'source',
-                globOptions: {
-                    dot: true
-                },
-                templateFiles: 'source/**'
-            }
-        ]
-    });
+module.exports = function(plop) {
+  plop.setHelper('pkgVersion', function() {
+    return pkg.version;
+  });
+  plop.setGenerator('CreateComponent', {
+    description: 'Create new component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter a name for your component:',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: path.join(process.cwd(), '{{name}}', '/src/lib/index.js'),
+        templateFile: 'templates/component.hbs',
+      },
+      {
+        type: 'add',
+        path: path.join(process.cwd(), '{{name}}', '/src/index.js'),
+        templateFile: 'templates/index.hbs',
+      },
+      {
+        type: 'add',
+        path: 'source/.npmignore',
+        templateFile: 'templates/npmignore.hbs',
+      },
+      {
+        type: 'add',
+        path: 'source/.gitignore',
+        templateFile: 'templates/gitignore.hbs',
+      },
+      {
+        type: 'add',
+        path: 'source/package.json',
+        templateFile: 'templates/package.hbs',
+      },
+      {
+        type: 'add',
+        path: 'source/webpack.config.js',
+        templateFile: 'templates/webpack.config.hbs',
+      },
+      {
+        type: 'add',
+        path: 'source/.tatareactrc',
+        templateFile: 'templates/tatareactrc.hbs',
+      },
+      {
+        type: 'addMany',
+        destination: path.join(process.cwd(), '{{name}}'),
+        base: 'source',
+        globOptions: {
+          dot: true,
+        },
+        templateFiles: 'source/**',
+      },
+    ],
+  });
 };
